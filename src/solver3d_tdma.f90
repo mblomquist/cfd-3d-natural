e8fc54a97b634e3c7882099a6d3ea2087408d081
Matt Blomquist <blomquist.matt@gmail.com>
2018-07-31 13:29:48 -0700

    Update solver3d_tdma.f90

--- src/solver3d_tdma.f90
+++ src/solver3d_tdma.f90
@@ -1,139 +1,249 @@
-! solver2d_tdma
+! solver3d_tdma
 !
 ! Written by Matt Blomquist
-! Last Update: 2018-05-15 (YYYY-MM-DD)
+! Last Update: 2018-07-18 (YYYY-MM-DD)
 !
-! This program solves a two-dimensional discretization problem utilizing a line-by-line
+! This program solves a three-dimensional discretization problem utilizing a line-by-line
 ! TDMA (tri-diagonal matrix algorithm).
 !
-subroutine solver3d_tdma(Aw, Ae, As, An, Ap, b, phi, m, n, tol, maxit)
+subroutine solver3d_tdma(Ab, As, Aw, Ap, Ae, An, At, b, phi, m, n, l, tol, maxit)
 
-  integer, intent(in) :: m, n, maxit
-  real(8), dimension(m,n), intent(in) :: Aw, Ae, As, An, Ap, b
-  real(8), dimension(m,n), intent(inout) :: phi
+  integer, intent(in) :: m, n, l, maxit
+  real(8), dimension(m,n,l), intent(in) :: Aw, Ae, As, An, At, Ab, Ap, b
+  real(8), dimension(m,n,l), intent(inout) :: phi
 
-  integer :: i, j, k
+  integer :: i, j, k, itr
   real(8), dimension(m) :: awe, bwe, cwe, dwe, phiwe
   real(8), dimension(n) :: asn, bsn, csn, dsn, phisn
-  real(8), dimension(m,n) :: r
+  real(8), dimension(l) :: abt, bbt, cbt, dbt, phibt
+  real(8), dimension(m,n,l) :: r
   real(8) :: r_sum, tol
 
-  do k = 1, maxit
+  do itr = 1,maxit
 
-    ! Start West - East Solve
-    do j = 1, n, 1
+    ! ==================== West - East ==================== !
+	do k = 1,l
+	  do j = 1,n
+	    do i = 1,m
 
-      do i = 1, m, 1
+		  awe(i) = Ap(i,j,k)
+		  bwe(i) = -Ae(i,j,k)
+		  cwe(i) = -Aw(i,j,k)
 
-	      awe(i) = Ap(i,j)
-	      bwe(i) = -Ae(i,j)
-	      cwe(i) = -Aw(i,j)
+		  if (j .eq. 1) then
 
-	      if (j .eq. 1) then
-	        dwe(i) = b(i,j)+An(i,j)*phi(i,j+1)
-	      elseif (j .eq. n) then
-	        dwe(i) = b(i,j)+As(i,j)*phi(i,j-1)
-	      else
-	        dwe(i) = b(i,j)+As(i,j)*phi(i,j-1)+An(i,j)*phi(i,j+1)
-	      end if
+		    if (k .eq. 1) then
+			  dwe(i) = b(i,j,k)+An(i,j,k)*phi(i,j+1,k)+At(i,j,k)*phi(i,j,k+1)
+			elseif (k .eq. l) then
+			  dwe(i) = b(i,j,k)+An(i,j,k)*phi(i,j+1,k)+Ab(i,j,k)*phi(i,j,k-1)
+			else
+			  dwe(i) = b(i,j,k)+An(i,j,k)*phi(i,j+1,k)+Ab(i,j,k)*phi(i,j,k-1)+At(i,j,k)*phi(i,j,k+1)
+			end if
 
-	    end do
+		  elseif (j .eq. n) then
 
-	    call solver1d_tdma(awe, bwe, cwe, dwe, phiwe, m)
+		    if (k .eq. 1) then
+			  dwe(i) = b(i,j,k)+As(i,j,k)*phi(i,j-1,k)+At(i,j,k)*phi(i,j,k+1)
+			elseif (k .eq. l) then
+			  dwe(i) = b(i,j,k)+As(i,j,k)*phi(i,j-1,k)+Ab(i,j,k)*phi(i,j,k-1)
+			else
+			  dwe(i) = b(i,j,k)+As(i,j,k)*phi(i,j-1,k)+Ab(i,j,k)*phi(i,j,k-1)+At(i,j,k)*phi(i,j,k+1)
+			end if
 
-	    phi(:,j) = phiwe(:)
+		  else
 
-    end do
+		    if (k .eq. 1) then
+			  dwe(i) = b(i,j,k)+As(i,j,k)*phi(i,j-1,k)+An(i,j,k)*phi(i,j+1,k)+At(i,j,k)*phi(i,j,k+1)
+			elseif (k .eq. l) then
+			  dwe(i) = b(i,j,k)+As(i,j,k)*phi(i,j-1,k)+An(i,j,k)*phi(i,j+1,k)+Ab(i,j,k)*phi(i,j,k-1)
+			else
+			  dwe(i) = b(i,j,k)+As(i,j,k)*phi(i,j-1,k)+An(i,j,k)*phi(i,j+1,k)+Ab(i,j,k)*phi(i,j,k-1)+At(i,j,k)*phi(i,j,k+1)
+			end if
 
-    ! Start South - North Solve
-    do i = 1, m, 1
+		  end if
 
-      do j = 1, n, 1
+		end do
 
-	    asn(j) = Ap(i,j)
-	    bsn(j) = -An(i,j)
-	    csn(j) = -As(i,j)
+    call solver1d_tdma(awe, bwe, cwe, dwe, phiwe, m)
 
-	    if (i .eq. 1) then
-	      dsn(j) = b(i,j)+Ae(i,j)*phi(i+1,j)
-	    elseif (i .eq. m) then
-	      dsn(j) = b(i,j)+Aw(i,j)*phi(i-1,j)
-	    else
-	      dsn(j) = b(i,j)+Aw(i,j)*phi(i-1,j)+Ae(i,j)*phi(i+1,j)
-	    end if
+    phi(:,j,k) = phiwe(:)
 
 	  end do
+	end do
 
-	  call solver1d_tdma(asn, bsn, csn, dsn, phisn, n)
+	! =================== South - North =================== !
+	do i = 1,m
+	  do k = 1,l
+	    do j = 1,n
 
- 	  phi(i,:) = phisn(:)
+		  asn(j) = Ap(i,j,k)
+		  bsn(j) = -An(i,j,k)
+		  csn(j) = -As(i,j,k)
 
-    end do
+		  if (k .eq. 1) then
 
-    ! Check Residual
-    r = 0
+		    if (i .eq. 1) then
+		      dsn(j) = b(i,j,k)+Ae(i,j,k)*phi(i+1,j,k)+At(i,j,k)*phi(i,j,k+1)
+		    elseif (i .eq. m) then
+		      dsn(j) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+At(i,j,k)*phi(i,j,k+1)
+		    else
+		      dsn(j) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+Ae(i,j,k)*phi(i+1,j,k)+At(i,j,k)*phi(i,j,k+1)
+		    end if
 
-    do j = 1, n
+		  elseif (k .eq. l) then
 
-      if (j .eq. 1) then
-        do i = 1, m
+		    if (i .eq. 1) then
+		      dsn(j) = b(i,j,k)+Ae(i,j,k)*phi(i+1,j,k)+Ab(i,j,k)*phi(i,j,k-1)
+		    elseif (i .eq. m) then
+		      dsn(j) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+Ab(i,j,k)*phi(i,j,k-1)
+		    else
+		      dsn(j) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+Ae(i,j,k)*phi(i+1,j,k)+Ab(i,j,k)*phi(i,j,k-1)
+		    end if
 
-          if (i .eq. 1) then
-            r(i,j) = Ap(i,j)*phi(i,j) - (Ae(i,j)*phi(i+1,j)+An(i,j)*phi(i,j+1)+b(i,j))
-          elseif (i .eq. m) then
-            r(i,j) = Ap(i,j)*phi(i,j) - (Aw(i,j)*phi(i-1,j)+An(i,j)*phi(i,j+1)+b(i,j))
-          else
-            r(i,j) = Ap(i,j)*phi(i,j) - (Aw(i,j)*phi(i-1,j)+Ae(i,j)*phi(i+1,j)+An(i,j)*phi(i,j+1)+b(i,j))
-          end if
+		  else
+
+		    if (i .eq. 1) then
+		      dsn(j) = b(i,j,k)+Ae(i,j,k)*phi(i+1,j,k)+Ab(i,j,k)*phi(i,j,k-1)+At(i,j,k)*phi(i,j,k+1)
+		    elseif (i .eq. m) then
+		      dsn(j) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+Ab(i,j,k)*phi(i,j,k-1)+At(i,j,k)*phi(i,j,k+1)
+		    else
+		      dsn(j) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+Ae(i,j,k)*phi(i+1,j,k)+Ab(i,j,k)*phi(i,j,k-1)+At(i,j,k)*phi(i,j,k+1)
+		    end if
+
+		  end if
+
+		end do
+
+    call solver1d_tdma(asn, bsn, csn, dsn, phisn, n)
+
+    phi(i,:,k) = phisn(:)
+
+	  end do
+	end do
+
+
+	! ===================== Bottom - Top ================== !
+	do j = 1,n
+	  do i = 1,m
+	    do k = 1,l
+
+		  abt(k) = Ap(i,j,k)
+		  bbt(k) = -At(i,j,k)
+		  cbt(k) = -Ab(i,j,k)
+
+		  if (j .eq. 1) then
+  			if (i .eq. 1) then
+  			  dbt(k) = b(i,j,k)+Ae(i,j,k)*phi(i+1,j,k)+An(i,j,k)*phi(i,j+1,k)
+  			elseif (i .eq. m) then
+  			  dbt(k) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+An(i,j,k)*phi(i,j+1,k)
+  			else
+  			  dbt(k) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+Ae(i,j,k)*phi(i+1,j,k)+An(i,j,k)*phi(i,j+1,k)
+  			end if
+		  elseif (j .eq. n) then
+  			if (i .eq. 1) then
+  			  dbt(k) = b(i,j,k)+Ae(i,j,k)*phi(i+1,j,k)+As(i,j,k)*phi(i,j-1,k)
+  			elseif (i .eq. m) then
+  			  dbt(k) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+As(i,j,k)*phi(i,j-1,k)
+  			else
+  			  dbt(k) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+Ae(i,j,k)*phi(i+1,j,k)+As(i,j,k)*phi(i,j-1,k)
+  			end if
+		  else
+  			if (i .eq. 1) then
+  			  dbt(k) = b(i,j,k)+Ae(i,j,k)*phi(i+1,j,k)+As(i,j,k)*phi(i,j-1,k)+An(i,j,k)*phi(i,j+1,k)
+  			elseif (i .eq. m) then
+  			  dbt(k) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+As(i,j,k)*phi(i,j-1,k)+An(i,j,k)*phi(i,j+1,k)
+  			else
+  			  dbt(k) = b(i,j,k)+Aw(i,j,k)*phi(i-1,j,k)+Ae(i,j,k)*phi(i+1,j,k)+As(i,j,k)*phi(i,j-1,k)+An(i,j,k)*phi(i,j+1,k)
+  			end if
+		  end if
+
+		end do
+
+    call solver1d_tdma(abt, bbt, cbt, dbt, phibt, l)
+
+    phi(i,j,:) = phibt(:)
+
+	  end do
+	end do
+
+	! =================== Check Solution ================== !
+  r = 0.
+
+	do i = 2,m-1
+	  do j = 2,n-1
+	    do k = 2,l-1
+
+			    r(i,j,k) = Ap(i,j,k)*phi(i,j,k)-(Aw(i,j,k)*phi(i-1,j,k)+ &
+				                                 Ae(i,j,k)*phi(i+1,j,k)+ &
+				                                 As(i,j,k)*phi(i,j-1,k)+ &
+				                                 An(i,j,k)*phi(i,j+1,k)+ &
+				                                 Ab(i,j,k)*phi(i,j,k-1)+ &
+				                                 At(i,j,k)*phi(i,j,k+1)+ &
+				                                 b(i,j,k))
 
         end do
-      elseif (j .eq. n) then
-        do i = 1, m
-
-          if (i .eq. 1) then
-            r(i,j) = Ap(i,j)*phi(i,j) - (Ae(i,j)*phi(i+1,j)+As(i,j)*phi(i,j-1)+b(i,j))
-          elseif (i .eq. m) then
-            r(i,j) = Ap(i,j)*phi(i,j) - (Aw(i,j)*phi(i-1,j)+As(i,j)*phi(i,j-1)+b(i,j))
-          else
-            r(i,j) = Ap(i,j)*phi(i,j) - (Aw(i,j)*phi(i-1,j)+Ae(i,j)*phi(i+1,j)+As(i,j)*phi(i,j-1)+b(i,j))
-          end if
-
-        end do
-      else
-        do i = 1, m
-
-          if (i .eq. 1) then
-            r(i,j) = Ap(i,j)*phi(i,j) - (Ae(i,j)*phi(i+1,j)+As(i,j)*phi(i,j-1)+An(i,j)*phi(i,j+1)+b(i,j))
-          elseif (i .eq. m) then
-            r(i,j) = Ap(i,j)*phi(i,j) - (Aw(i,j)*phi(i-1,j)+As(i,j)*phi(i,j-1)+An(i,j)*phi(i,j+1)+b(i,j))
-          else
-            r(i,j) = Ap(i,j)*phi(i,j) - (Aw(i,j)*phi(i-1,j)+Ae(i,j)*phi(i+1,j)+As(i,j)*phi(i,j-1)+An(i,j)*phi(i,j+1)+b(i,j))
-          end if
-
-        end do
-      end if
-
-    end do
-
-    r_sum = 0
-
-    do j = 1, n
-      do i = 1, m
-        r_sum = r_sum + abs(r(i,j))
       end do
     end do
 
-    !print *, "r_sum, itr:", r_sum, k
+	r_sum = 0.
 
-    if (r_sum < tol) then
-      !print *, "TDMA Compelete."
-      !print *, "r_sum:", r_sum
-      !print *, "itrs:", k
+	do i = 2,m-1
+	  do j = 2,n-1
+	    do k = 2,l-1
+		    r_sum = r_sum + (r(i,j,k))**2.0
+		  end do
+	  end do
+	end do
+
+  r_sum = r_sum**(0.5)
+  
+	if (r_sum .le. tol) then
+	  print *, "TDMA Compelete."
+    print *, "r_sum:", r_sum
+    print *, "itrs:", itr
       return
-    end if
+  else
+    print *, "r_sum:", r_sum
+    print *, "itrs:", itr
+	end if
 
   end do
 
   return
 
 end subroutine solver3d_tdma
+
+subroutine solver1d_tdma(a, b, c, d, phi, n)
+
+  ! Define input / output variables
+  integer, intent(in) :: n
+  real(8), intent(in) :: a(n), b(n), c(n), d(n)
+  real(8), intent(out) :: phi(n)
+
+  ! Define internal variables
+  real(8), dimension(n) :: P, Q
+
+  integer :: i
+
+  ! Start forward-substitution
+  P(1) = b(1)/a(1)
+  Q(1) = d(1)/a(1)
+
+  do i = 2, n, 1
+    P(i) = b(i)/(a(i)-c(i)*P(i-1))
+    Q(i) = (d(i)-c(i)*Q(i-1))/(a(i)-c(i)*P(i-1))
+
+  end do
+
+  phi(n) = Q(n)
+
+  ! Start backward-substitution
+  do i = n-1, 1, -1
+
+    phi(i) = Q(i)-P(i)*phi(i+1)
+
+  end do
+
+  return
+
+end subroutine solver1d_tdma
